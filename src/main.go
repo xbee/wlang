502fd9855166f6d6970977538881c63bfa42a114
xbee <hfeeki@outlook.com>
2020-02-06 10:42:06 +0800

    add src

---
+++ src/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"ast"
+	"fmt"
+
+	// "github.com/conseweb/wlang"
+	"parser"
+	"token"
+)
+
+func main() {
+	fset := token.NewFileSet() // positions are relative to fset
+
+	src := `package foo
+
+import (
+	"fmt"
+	"time"
+)
+
+func bar() {
+	fmt.Println(time.Now())
+}`
+
+	// Parse src but stop after processing the imports.
+	f, err := parser.ParseFile(fset, "", src, 0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Print the imports from the file's AST.
+	for _, s := range f.Imports {
+		fmt.Println(s.Path.Value)
+	}
+
+	// Inspect the AST and print all identifiers and literals.
+	ast.Inspect(f, func(n ast.Node) bool {
+		var s string
+		switch x := n.(type) {
+		case *ast.BasicLit:
+			s = x.Value
+		case *ast.Ident:
+			s = x.Name
+		}
+		if s != "" {
+			fmt.Printf("%s:\t%s\n", fset.Position(n.Pos()), s)
+		}
+		return true
+	})
+
+	ast.Print(fset, f)
+
+	// output:
+	//
+	// "fmt"
+	// "time"
+}
